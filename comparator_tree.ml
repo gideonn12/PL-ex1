30837410f8db7584725b06925402df866a755041
ilanitb16 <ilanit.berdichevski@gmail.com>
2024-11-16 18:43:25 +0200

    add second part

---
+++ comparator_tree.ml
@@ -0,0 +1,12 @@
+type 'a binary_tree = 
+  | Empty
+  | Node of 'a * 'a binary_tree * 'a binary_tree
+
+let rec comparator_tree tree elem comparator =
+  match tree with
+  | Empty -> Node (elem, Empty, Empty)
+  | Node (value, left, right) ->
+      if comparator elem value <= 0 then
+        Node (value, comparator_tree left elem comparator, right)
+      else
+        Node (value, left, comparator_tree right elem comparator)
