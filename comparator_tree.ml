0a7ca045a077aa48bcd0f81efb6f2ba142a5aba7
gideonn12 <gideoneeman@icloud.com>
2024-11-25 18:43:36 +0200

    changed to false

--- comparator_tree.ml
+++ comparator_tree.ml
@@ -6,7 +6,7 @@ let rec comparator_tree tree elem comparator =
   match tree with
   | Empty -> Node (elem, Empty, Empty)
   | Node (value, left, right) ->
-      if comparator elem value <= true then
+      if comparator elem value <= false then
         Node (value, comparator_tree left elem comparator, right)
       else
         Node (value, left, comparator_tree right elem comparator);;
